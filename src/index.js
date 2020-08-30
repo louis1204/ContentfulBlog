56bd1c4e9af5305599fb94e5502e77c6389666c2
Louis <Louis1204@gmail.com>
2020-08-30 13:55:02 -0700

    More cleaning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
 import App from "./component/App";
 
 ReactDOM.render((
-    <Router>
-      <App />
-    </Router>
+    <App />
   ), document.getElementById('root'));
